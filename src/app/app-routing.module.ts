0a42e567d4c9366e989756d03225d71fc669a2b1
cooperhc <69803510+cooperhc@users.noreply.github.com>
2023-04-18 23:31:52 -0400

    First commit, GH page test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisComponent } from './vis/vis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VisComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/vis/vis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vis',
+  templateUrl: './vis.component.html',
+  styleUrls: ['./vis.component.css']
+})
+export class VisComponent {
+
+}
